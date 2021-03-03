61f85188f634961dc3802168819024e8f98c5670
Cansın Aldanmaz <canald@gmail.com>
2021-03-03 14:12:49 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+set -ex
+echo "Creating cluster"
+kind create cluster --config=./config/kind.yaml
+echo "Installing ingress"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+echo "Installing cert manager"
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.yaml
+echo "Installing certs"
+kubectl apply -f ./certs/cert-issuer.yaml
