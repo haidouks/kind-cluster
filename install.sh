28617a9f14e205bc99008ebc8eaa4510d878d463
Cansın Aldanmaz <canald@gmail.com>
2021-03-03 19:43:51 +0300

    changed ports

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ set -ex
 echo "Creating cluster"
 kind create cluster --config=./config/kind.yaml --name test
 echo "Installing ingress"
-kubectl apply --validate=true -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
 echo "Installing cert manager"
-kubectl apply --validate=true -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.yaml
 echo "Installing certs"
 kubectl apply --validate=true -f ./certs/cert-issuer.yaml
